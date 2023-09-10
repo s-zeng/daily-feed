9ca10eee39f1405d51da1a7509399532549a740b
Simon Zeng <contact@simonzeng.com>
2023-09-10 15:37:00 -0400

    Minor change: use qualified import for rss

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,8 +1,7 @@
-use rss::Channel;
 use std::error::Error;
 
-pub async fn feed_from_url(url: &str) -> Result<Channel, Box<dyn Error>> {
+pub async fn feed_from_url(url: &str) -> Result<rss::Channel, Box<dyn Error>> {
     let content = reqwest::get(url).await?.bytes().await?;
-    let channel = Channel::read_from(&content[..])?;
+    let channel = rss::Channel::read_from(&content[..])?;
     Ok(channel)
 }
